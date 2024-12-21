f3be196280310eb50bb1c45f7d240fa7795e5259
filip <filip.chromecka@outlook.com>
2024-12-22 00:46:22 +0100

    Add language search filter

--- src/app/compilations/compilations.page.ts
+++ src/app/compilations/compilations.page.ts
@@ -3,19 +3,23 @@ import { Router } from '@angular/router';
 import { StorageService } from '../services/storage.service';
 import { Compilation } from '../models/compilation';
 import { SAVED_COMPILATIONS } from '../app.constants';
+import { Language, LanguageDisplayNames } from '../models/languages';
 
 @Component({
   selector: 'compilations',
   templateUrl: 'compilations.page.html',
   styleUrls: ['compilations.page.scss'],
-  standalone: false,
+  standalone: false
 })
 export class CompilationsPage implements OnInit {
+  public languageDisplayNames = LanguageDisplayNames;
   compilations: Compilation[] | null = null;
   filteredCompilations: Compilation[] | null = null;
   searchTerm: string = '';
+  selectedLanguage: Language | null = null;
+  languages = [null, ...Object.values(Language)];
 
-  constructor(    
+  constructor(
     private router: Router,
     private storageService: StorageService
   ) {}
@@ -25,9 +29,7 @@ export class CompilationsPage implements OnInit {
   async ionViewWillEnter() {
     this.compilations = await this.storageService.get(SAVED_COMPILATIONS);
     if (this.compilations) {
-      this.compilations.sort((a: Compilation, b: Compilation) => {
-        return b.timestamp.getTime() - a.timestamp.getTime();
-      });
+      this.compilations.sort((a: Compilation, b: Compilation) => b.timestamp.getTime() - a.timestamp.getTime());
       this.filteredCompilations = this.compilations;
     }
   }
@@ -38,11 +40,21 @@ export class CompilationsPage implements OnInit {
 
   onSearchChange(event: any) {
     const query = event.target.value.toLowerCase();
+    this.applyFilters(query, this.selectedLanguage);
+  }
 
+  onLanguageChange(event: any) {
+    this.selectedLanguage = event.detail.value;
+    this.applyFilters(this.searchTerm, this.selectedLanguage);
+  }
+
+  applyFilters(searchTerm: string, language: Language | null) {
     if (this.compilations) {
-      this.filteredCompilations = this.compilations.filter(compilation => 
-        compilation.title.toLowerCase().includes(query)
-      );
+      this.filteredCompilations = this.compilations.filter(compilation => {
+        const matchesSearchTerm = compilation.title.toLowerCase().includes(searchTerm.toLowerCase());
+        const matchesLanguage = language === null || compilation.request.language === language;
+        return matchesSearchTerm && matchesLanguage;
+      });
     }
   }
 }
